4d7a28a9ce72b61e9225086400948d2aed34c62a
daniellongi <djvildosola@uc.cl>
2024-04-25 04:10:41 -0400

    dotenv package

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
 const app = express()
-const port = 9000
+const port = process.env.PORT
 
 
 app.get('/', (req, res) => {
