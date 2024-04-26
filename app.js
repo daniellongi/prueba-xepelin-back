0d1831d2064432a1c4a8901b734a68c544c6cd72
daniellongi <djvildosola@uc.cl>
2024-04-26 04:09:53 -0400

    fixes

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import axios from 'axios';
-import writeContentInGoogleSheets from './functions/update-google-sheets.js';
-import scrapReadTime from './functions/scrap-read-time.js';
-import formatDateToDDMMYYYY from './functions/format-time.js';
+import writeContentInGoogleSheets from './functions/updateGoogleSheets.js';
+import scrapReadTime from './functions/scrapReadTime.js';
+import formatDateToDDMMYYYY from './functions/formatTime.js';
 import getTotalBlogsInCategory from './functions/getTotalBlogsInCategory.js';
 import sendGoogleSheetToWebHook from './functions/webhookPost.js';
 
@@ -24,9 +24,9 @@ app.post('/p2-prueba-xepelin', async (req, res) => {
     const { category, webhookUrl } = req.body;
     if (!validCategories.includes(category)) return res.status(400).send("La categoria ingresada no existe");
     const totalBlogsInCategory =  await getTotalBlogsInCategory(category);
-    const url = `https://4n68r2aa.apicdn.sanity.io/v2023-08-21/data/query/production?query=*%0A%09++%09%5B%0A%09%09%09%09%28_type+%3D%3D+%22blogArticle%22%29%0A++++++++%0A++++++++%26%26+%28%0A%09%09%09%09%09blogCategory-%3Eslug.current+%3D%3D+%22${category}%22%0A%09%09%09%09%29%0A%09%09++%5D+%0A%09%09%09%7C+order%28date+desc%2C+_createdAt+desc%29%0A%09%09%09%7B%0A++++++_type%2C%0A++++++_id%2C%0A++++++slug%2C%0A++++++title%2C%0A%09%09%09excerpt%2C%0A++++++featuredImage%2C%0A++++++_createdAt%2C%0A++++++_updatedAt%2C%0A++++++date%2C%0A++++++author-%3E%2C%0A++++++blogCategory-%3E%2C%0A++++++blogSubCategory-%3E%2C%0A++++++resourceCategory-%3E%2C%0A++++%7D%5B0...${totalBlogsInCategory}%5D&returnQuery=false&perspective=published`;
-    const response = await axios.get(url);
-    const googleSheetValuesPromises = response.data.result.map( async (blog)=>{
+    const urlToScrap = `https://4n68r2aa.apicdn.sanity.io/v2023-08-21/data/query/production?query=*%0A%09++%09%5B%0A%09%09%09%09%28_type+%3D%3D+%22blogArticle%22%29%0A++++++++%0A++++++++%26%26+%28%0A%09%09%09%09%09blogCategory-%3Eslug.current+%3D%3D+%22${category}%22%0A%09%09%09%09%29%0A%09%09++%5D+%0A%09%09%09%7C+order%28date+desc%2C+_createdAt+desc%29%0A%09%09%09%7B%0A++++++_type%2C%0A++++++_id%2C%0A++++++slug%2C%0A++++++title%2C%0A%09%09%09excerpt%2C%0A++++++featuredImage%2C%0A++++++_createdAt%2C%0A++++++_updatedAt%2C%0A++++++date%2C%0A++++++author-%3E%2C%0A++++++blogCategory-%3E%2C%0A++++++blogSubCategory-%3E%2C%0A++++++resourceCategory-%3E%2C%0A++++%7D%5B0...${totalBlogsInCategory}%5D&returnQuery=false&perspective=published`;
+    const scrappingResults = await axios.get(urlToScrap);
+    const googleSheetValuesPromises = scrappingResults.data.result.map( async (blog)=>{
       const title = blog.slug.current;
       const author = blog.author.name;
       const blogCategory = blog.blogCategory.frontendId;
